0acc903ed0428b903198c91eef70c739671f5420
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-08-10 18:08:44 +0300

    update AS, add as-pect with tests, fix bugs

--- dice-game/backend-as/assembly/game_handler.ts
+++ dice-game/backend-as/assembly/game_handler.ts
@@ -5,6 +5,11 @@ import {log} from "./logger";
 
 let gameManager = new GameManager();
 
+function errorResponse(msg: string): string {
+    let error = new ErrorResponse(msg);
+    return error.serialize();
+}
+
 // returns string, because serialization to a byte array is not compatible with our invoke handlers
 export function handler(requestBytes: Uint8Array): string {
 
@@ -16,24 +21,27 @@ export function handler(requestBytes: Uint8Array): string {
     } else if (request.action == Action.Roll) {
         log("Handling Roll request\n");
         let r = request as RollRequest;
+        if (r.betPlacementExists == false || r.betSizeExists == false || r.playerIdExists == false) {
+            let msg = "Request does not match schema. Missing fields: ";
+            if (r.betPlacementExists == false) msg = msg + "'bet_placement',";
+            if (r.betSizeExists == false) msg = msg + "'bet_size',";
+            if (r.playerIdExists == false) msg = msg + "'player_id'";
+            return errorResponse(msg)
+        }
         return gameManager.roll(r.playerId, r.betPlacement, r.betSize);
     } else if (request.action == Action.GetBalance) {
         log("Handling GetBalance request\n");
         let r = request as GetBalanceRequest;
+        if (r.playerIdExists == false) {
+            return errorResponse("Request does not match schema. Required fields: 'player_id'")
+        }
         return gameManager.getBalance(r.playerId);
     } else if (request.action == Action.Unknown) {
         log("Unknown request\n");
         let r = request as UnknownRequest;
-        let error = new ErrorResponse(r.message);
-        let returnStr = error.serialize();
-        memory.free(changetype<usize>(error));
-        return returnStr;
+        return errorResponse(r.message)
     }
 
-    memory.free(changetype<usize>(request));
-
     let response = new ErrorResponse("Unreachable.");
-    let returnStr = response.serialize();
-    memory.free(changetype<usize>(response));
-    return returnStr;
+    return response.serialize();
 }
--- dice-game/backend-as/assembly/index.ts
+++ dice-game/backend-as/assembly/index.ts
@@ -1,16 +1,11 @@
-// import "allocator/tlsf";
-// import "collector/itcm";
-import "allocator/buddy";
-// import "allocator/arena";
-
 import {handler} from "./game_handler";
 
 export function allocate(size: i32) :i32 {
-  return memory.allocate(size);
+  return __alloc(size, 1);
 }
 
 export function deallocate(ptr: i32, size: i32): void {
-  memory.free(ptr);
+  __free(ptr)
 }
 
 export function invoke(ptr: i32, size: i32): i32 {
@@ -24,7 +19,7 @@ export function invoke(ptr: i32, size: i32): i32 {
     let result = handler(bb);
 
     let strLen: i32 = result.length;
-    let addr = memory.allocate(strLen + 4);
+    let addr = __alloc(strLen + 4, idof<Array<u8>>());
     for (let i = 0; i < 4; i++) {
       let b: u8 = (strLen >> i * 8) as u8 & 0xFF;
       store<u8>(addr + i, b);
@@ -35,11 +30,6 @@ export function invoke(ptr: i32, size: i32): i32 {
         let b: u8 = result.charCodeAt(i) as u8;
       store<u8>(strAddr + i, b);
     }
-
-    memory.free(changetype<usize>(result));
-    memory.free(ptr);
-    memory.free(changetype<usize>(bb));
-    memory.free(changetype<usize>(bb.buffer));
 
     return addr;
 }
