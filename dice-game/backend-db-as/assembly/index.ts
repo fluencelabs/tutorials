3b3991de2124a07db42047d665a891a18fc45324
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-08-12 20:17:48 +0300

    the multimodule dice game is compiled

--- dice-game/backend-db-as/assembly/index.ts
+++ dice-game/backend-db-as/assembly/index.ts
@@ -1,15 +1,13 @@
-import "allocator/arena";
-
 import {handler} from "./game_handler";
 import {loggedStringHandler} from "../node_modules/assemblyscript-sdk/assembly/index";
 import {log} from "../node_modules/assemblyscript-sdk/assembly/logger";
 
 export function allocate(size: usize) :i32 {
-  return memory.allocate(size);
+  return __alloc(size, 1);
 }
 
 export function deallocate(ptr: i32, size: usize): void {
-  memory.reset();
+
 }
 
 export function invoke(ptr: i32, size: i32): i32 {
